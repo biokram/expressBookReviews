aca7d50371e1971b7d3644eb6f631e5d9120acf0
biokram <mark.elmore87@gmail.com>
2025-07-21 14:39:14 -0400

    corrected typo error in index.js

--- final_project/index.js
+++ final_project/index.js
@@ -1,4 +1,4 @@
-onst express = require('express');
+const express = require('express');
 const jwt = require('jsonwebtoken');
 const session = require('express-session')
 const customer_routes = require('./router/auth_users.js').authenticated;
